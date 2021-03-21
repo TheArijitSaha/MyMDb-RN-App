355768715c590461a424ec35b77021b5bc8aab54
Arijit Saha <arijitbiley@gmail.com>
2021-03-21 09:50:04 +0530

    Shifted PHP code to legacy/PHP branch

--- PHP Localhost Website/async/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once('../classes/variables.php');
-
-    header("Location: ".NetworkVariables::$home_path);
-    exit();
-?>
--- PHP Localhost Website/classes/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once('./variables.php');
-
-    header("Location: ".NetworkVariables::$home_path);
-    exit();
-?>
