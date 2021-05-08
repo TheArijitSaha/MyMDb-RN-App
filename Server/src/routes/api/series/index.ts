2282281429aa3c931fc5beca47b7550e49198c5a
Arijit Saha <arijitbiley@gmail.com>
2021-05-08 19:08:53 +0530

    Add series scraper

--- Server/src/routes/api/series/index.ts
+++ Server/src/routes/api/series/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 
+import axios from "axios";
 import chalk from "chalk";
+import { load } from "cheerio";
 
 import Auth from "../../auth";
 import Series from "../../../models/Series";
@@ -170,6 +172,47 @@ router.get(
   }
 );
 
+// POST /:id - create a series
+router.post(
+  "/",
+  Auth.required,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { series } = req.body;
+
+    // If any of the two required body parameters (title, timeSpan.start) are
+    // missing then send Status 422: Unprocessable Entity
+    if (!series) {
+      res.status(422).json("required body parameter 'series' missing");
+      return;
+    }
+    if (!series.title) {
+      res.status(422).json("required series parameter 'title' missing");
+      return;
+    }
+    if (!series.timeSpan.start) {
+      res
+        .status(422)
+        .json("required series parameter 'timeSpan.start' missing");
+      return;
+    }
+
+    try {
+      const data = await Series.create({ ...series });
+
+      res.status(201).json(data);
+    } catch (e) {
+      // If the error is caused due to duplicate series entry being requested,
+      // return Response Status 409: Conflict
+      if (e.name === "MongoError" && e.code === 11000) {
+        res.status(409).json("Duplicate series");
+        return;
+      }
+
+      next(e);
+    }
+  }
+);
+
 // PATCH /:id - update a series
 router.patch(
   "/:id",
@@ -180,7 +223,7 @@ router.patch(
     if (!req.body.series || !(req.body.series._id === id)) {
       res.status(422).json({
         error:
-          "The Movie object with the unique id must be given to identify it.",
+          "The Series object with the unique id must be given to identify it.",
       });
       return;
     }
@@ -207,6 +250,82 @@ router.patch(
       res.json(data);
     } catch (e) {
       console.error(e);
+      next(e);
+    }
+  }
+);
+
+// GET /scrape/imdb - fetches scraped data from IMDB
+router.get(
+  "/scrape/imdb",
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.query.link) {
+      res.status(400).send("No link to scrape");
+      return;
+    }
+
+    const link = req.query.link.toString();
+
+    try {
+      const response = await axios.get(link.replace("m.imdb", "imdb"), {
+        headers: { "Accept-Language": "en-US,en;" },
+      });
+
+      const $ = load(response.data, { xmlMode: true, decodeEntities: false });
+
+      let imdbSeries = JSON.parse(
+        $('script[type="application/ld+json"]').eq(0).html()
+      );
+
+      const title = imdbSeries.name;
+
+      let creators = [];
+      if (Array.isArray(imdbSeries.creator)) {
+        for (let cre of imdbSeries.creator) {
+          if (cre["@type"] === "Person") creators.push(cre.name);
+        }
+      } else {
+        if (imdbSeries.creator["@type"] === "Person") {
+          creators.push(imdbSeries.creator.name);
+        }
+      }
+
+      let cast = [];
+      if (Array.isArray(imdbSeries.actor)) {
+        for (let actor of imdbSeries.actor) {
+          cast.push(actor.name);
+        }
+      } else {
+        cast.push(imdbSeries.actor.name);
+      }
+
+      let genres = [];
+      if (Array.isArray(imdbSeries.genre)) {
+        for (let i = 0; i < Math.min(imdbSeries.genre.length, 3); ++i) {
+          genres.push(imdbSeries.genre[i]);
+        }
+      } else {
+        genres.push(imdbSeries.genre);
+      }
+
+      // TODO: find a way to scrape these things
+      // meanRuntime
+      // rottenTomatoes
+      // timeSpan.end
+      const scrapedSeries = {
+        title,
+        timeSpan: { start: parseInt(imdbSeries.datePublished, 10) },
+        creators,
+        cast,
+        genres,
+        imdb: {
+          rating: parseFloat(imdbSeries.aggregateRating.ratingValue),
+          link,
+        },
+      };
+
+      res.json(scrapedSeries);
+    } catch (e) {
       next(e);
     }
   }
