541d51d817bf46c26f1cfc2dfdbf855a1be5d0f1
Arijit Saha <arijitbiley@gmail.com>
2021-05-29 21:05:47 +0530

    Add heroku deploy script

---
+++ deploy-heroku.sh
@@ -0,0 +1,11 @@
+git subtree --prefix=Server push heroku master
+
+# If ever a deployment fails, then use this from then on
+#cd qnq-backend/
+#git init
+#git add .
+#git commit -am "deploy"
+#git push --force https://git.heroku.com/qnq-backend.git master
+#rm -rf .git
+#cd ..
+
